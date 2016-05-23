e636b5a78689b7aa9f18a7ce0d8ef9e4dfcb7b84
gabriel vareilles <gabriel.vareilles@gmail.com>
2016-05-23 17:41:40 +0200

    added show.erb feature for flats

--- config.rb
+++ config.rb
@@ -89,3 +89,7 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+["claudie", "borat", "jeanjean"].each do |name|
+  proxy "/weekends/#{name}.html", "/weekends/show.html", :locals => { :owner => name }, :ignore => true
+end
